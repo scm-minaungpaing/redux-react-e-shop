ec51e08214bb1758deb3f0ecf27270c3e192a513
scm-minaungpaing <min.aungpaing@seattleconsultingmyanmar.com>
2022-03-23 00:00:53 +0630

    for testing deploy

--- src/page/Products.jsx
+++ src/page/Products.jsx
@@ -7,20 +7,20 @@ const Product = () => {
     const [data, setData] = useState([]);
     const [filter, setFilter] = useState([data]);
     const [loading, setLoading] = useState([false]);
-    let componentMounted = true;
+    // let componentMounted = true;
 
     useEffect(() => {
         const getProducts = async () => {
             setLoading(true)
             const response = await fetch("https://fakestoreapi.com/products")
-            if (componentMounted) {
+            // if (componentMounted) {
                 setData(await response.clone().json())
                 setFilter(await response.json())
                 setLoading(false)
-            }
-            return () => {
-                componentMounted = false
-            }
+            // }
+            // return () => {
+            //     componentMounted = false
+            // }
         }
         getProducts()
     }, [])
